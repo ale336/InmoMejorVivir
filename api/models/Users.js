67f98ef1c30165e1018a26c5e872ebcc9a8e54ef
Alejandro Ramos <caramos336@gmail.com>
2022-09-14 15:19:05 -0300

    Only RegisterUsers

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,9 +1,14 @@
 const Sequelize = require ("sequelize");
 const db = require ("../config/db");
+const bcrypt = require("bcrypt");
 //const Users = require("../models");
 
 
-class Users extends Sequelize.Model {}
+class Users extends Sequelize.Model {
+    hash(password,salt) {
+        return bcrypt.hash(password,salt)
+    }
+}
 
 Users.init (    
     {
@@ -23,8 +28,22 @@ Users.init (
             type : Sequelize.STRING,
             allowNull:false,
         },
+        salt : {
+            type : Sequelize.STRING,
+        },
     },
     {sequelize:db,modelName:"users"}
 );
+
+Users.beforeCreate((user)=>{
+    const salt = bcrypt.genSaltSync();
+    user.salt = salt;
+    return user.hash(user.password,salt)
+            .then(hash=>{
+                user.password = hash
+            });
+});
+
+
 
 module.exports = Users;
--- src/App.js
+++ src/App.js
@@ -1,31 +1,100 @@
+import { useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {  
-  return (
-    <div className="App">
-        <form>
-        <h3>Registro de Usuarios</h3>
-        <div>
-            <label>Nombre</label>
-            <input class="entradas" type="text" id="name" placeholder="Ingrese su nombre" ></input>
-        </div>
-        <div>
-            <label>Apellido</label>
-            <input class="entradas" type="text" id="lastname" placeholder="Ingrese su apellido" ></input>
-        </div>
-        <div>
-            <label>E-mail</label>
-            <input class="entradas" type="text" id="email" placeholder="Ingrese su e-mail" ></input>
-        </div>
-        <div>
-            <label>Password</label>
-            <input class="entradas" type="text" id="password" placeholder="Ingrese su password" ></input>
-        </div>
-        <input class="boton" type="submit" value="Registrar"></input>
-    </form>
-    </div>
     
+    const [name,setName] = useState("")
+    const [lastname,setLastname] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const handleNameChange =(e)=>{
+        setName (e.target.value)
+    }
+    const handleLastnameChange =(e)=>{
+        setLastname (e.target.value)
+    }
+    const handleEmailChange =(e)=>{
+        setEmail (e.target.value)
+    }
+    const handlePasswordChange=(e)=>{
+        setPassword(e.target.value)
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post("http://localhost:3001/api/users/register",{name,lastname,email,password})
+            .then((res)=>res.data)
+            .then((user)=>console.log("NUEVO USUARIO",user));
+    }
+
+    return (
+    <div className="App">
+        <form onSubmit={handleSubmit}>
+            <h3>Registro de Usuarios</h3>
+            <div>
+                <label>Nombre</label>
+                <input 
+                    className="entradas" 
+                    type="text" 
+                    placeholder="Ingrese su nombre" 
+                    onChange={handleNameChange} 
+                    value={name}>
+                </input>
+            </div>
+            <div>
+                <label>Apellido</label>
+                <input 
+                    className="entradas" 
+                    type="text"  
+                    placeholder="Ingrese su apellido" 
+                    onChange={handleLastnameChange} 
+                    value={lastname}>
+                </input>
+            </div>
+            <div>
+                <label>E-mail</label>
+                <input 
+                    className="entradas" 
+                    type="email" 
+                    placeholder="Ingrese su e-mail" 
+                    onChange={handleEmailChange} 
+                    value={email}>
+                </input>
+            </div>
+            <div>
+            <label>Password</label>
+            <input 
+                className="entradas" 
+                type="password" 
+                placeholder="Ingrese su password" 
+                onChange={handlePasswordChange} 
+                value={password}>
+            </input>
+            </div>
+            <button 
+                className='boton' 
+                type="submit" 
+                value="Registrar">REGISTRAR</button>
+        </form>
+    </div>
+
   );
 }
 
 export default App;
+
+//PLEDU FORMULARIO
+//     <div className="layout m-5">
+//     <h3 className="title is-3">New Playlist</h3>
+//     <form>
+//       <label className="label my-3">Name</label>
+//       <input className="input" type="text" placeholder="Playlist name" />
+
+//       <label className="label my-3">Description</label>
+//       <textarea className="textarea" placeholder="Description"></textarea>
+//       <button className="button is-link my-5">Submit</button>
+//     </form>
+//   </div>
